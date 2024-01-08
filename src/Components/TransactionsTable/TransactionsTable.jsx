8889012fd92aa5bd1a81bc9b29883acb1680db3b
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-09 02:46:01 +0530

    feat: added table head

--- src/Components/TransactionsTable/TransactionsTable.jsx
+++ src/Components/TransactionsTable/TransactionsTable.jsx
@@ -1,3 +1,4 @@
+import { TransactionsTableHead } from "../TransactionsTableHead/TransactionsTableHead";
 import { TransactionsTableHeader } from "../TransactionsTableHeader/TransactionsTableHeader";
 import styles from "./transactionsTable.module.scss";
 
@@ -5,6 +6,10 @@ export const TransactionsTable = () => {
     return (
         <div className={styles.transactions_table_container}>
             <TransactionsTableHeader />
+            <table>
+                <TransactionsTableHead />
+                <p>a</p>
+            </table>
         </div>
     );
 };
---
+++ src/Components/TransactionsTableHead/TransactionsTableHead.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import styles from "./transactionsTableHead.module.scss";
+
+export const TransactionsTableHead = () => {
+    const [isTransactionFeesClicked, setIsTransactionFeesClicked] =
+        useState(false);
+
+    const handleTransactionFeesClicked = () => {
+        setIsTransactionFeesClicked((prev) => !prev);
+    };
+
+    return (
+        <thead className={styles.table_head}>
+            <tr className={styles.table_head_tr}>
+                <th className={styles.table_head_th}>Order ID</th>
+                <th className={styles.table_head_th}>
+                    Order date{" "}
+                    <img
+                        src="/src/assets/icons/DownArrowFilled.svg"
+                        alt="arrow"
+                        className={styles.th_img}
+                    />
+                </th>
+                <th className={styles.table_head_th}>Order amount</th>
+                <th
+                    className={styles.table_head_th}
+                    onClick={handleTransactionFeesClicked}
+                >
+                    Transaction fees
+                    <img
+                        src="/src/assets/icons/Info.svg"
+                        alt="info"
+                        className={styles.th_img}
+                    />
+                    {isTransactionFeesClicked && (
+                        <p className={styles.transaction_fees_popup}>
+                            Transaction fees are charged as a percentage of the
+                            order amount according to your plan.
+                        </p>
+                    )}
+                </th>
+            </tr>
+        </thead>
+    );
+};
